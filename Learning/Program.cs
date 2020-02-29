c8833f2b847216b3a83c7447c31a4eabed091f39
zdk545652663 <aimeng0521@gmail.com>
2020-02-29 15:33:51 +0800

    5-1

---
+++ Learning/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private int index = 1;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine($"{123424.41324132,40:N4}");
+            //Console.WriteLine($"{12123,-4:X5}");
+            //Console.WriteLine("The money is {0}", 500);
+            //Console.WriteLine("The money is {0:C}", 500);
+            //Console.WriteLine($"Pi is { Math.PI:f2}");
+
+
+            int i = 3;
+            int j = 6;
+            Console.WriteLine("Hi there{1}，{0}","zhes 000","zhes 111");
+            Console.WriteLine($"这个是{i}JHe{j}");
+
+            Console.Write("1");
+            Console.Write("1");
+            Console.Write("1");
+            while (true)
+            {
+            } 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private bool Return1()
+        {
+            bool i;
+            return i;
+        }
+    }
+}
